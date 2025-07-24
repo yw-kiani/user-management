a638ca95135eac8887eff52d6ec7c6c35fe231fb
yw-kiani <161882750+yw-kiani@users.noreply.github.com>
2025-07-24 13:44:05 -0700

    mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,14 +3,14 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'memonabdulwasiu1@gmail.com',
+    user: 'anonymous03146@gmail.com',
     pass: 'uhur jmgf pubo uufs'
   }
 });
 
 const sendAlertEmail = async (to, subject, text) => {
   await transporter.sendMail({
-    from: '"Security Alert" <memonabdulwasiu1@gmail.com>',
+    from: '"Security Alert" <anonymous03146@gmail.com>',
     to,
     subject,
     text
